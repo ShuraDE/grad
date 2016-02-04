da43968c1d06b65a8b4dd9cc7b061a1bfa0993d7
fry-grad <poehlmannmike@yahoo.de>
2016-02-04 21:53:47 +0100

    add work macro

---
+++ ga_macro.hpp
@@ -0,0 +1,9 @@
+﻿#define PRETAG grad
+#define INTTAG bis  
+#define DOUSET(se1,se2) se1##_##se2
+#define TRISET(se1,se2,se3) se1##_##se2##_##se3
+#define SFUNC(se1) TRISET(PRETAG,fnc,se1)
+#define SVAR(se1) DOUSET(PRETAG,se1)
+#define IFUNC(se1) TRISET(INTTAG,fnc,se1)
+#define CPORT DOUSET(fwm,comport)
+#define CNAME(se1) #DOUSET(PRETAG,se1)
