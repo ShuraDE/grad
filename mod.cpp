98f7ef1cf1eb25a81df5fc8a38fe1c8d2b6c156b
ShuraDE <mail@kev-in.net>
2016-02-04 21:57:51 +0100

    logo

---
+++ mod.cpp
@@ -0,0 +1,11 @@
+name = "Gruppe Adler Libary v0.0.1";
+picture = "ga_logo.paa";
+actionName = "Website";
+action = "http://gruppe-adler.de";
+logo = "ga_logo.paa";
+logoOver = "ga_logo.paa";
+tooltip = "Gruppe Adler Libary";
+tooltipOwned = "Gruppe Adler Libary Owned";
+author = "Gruppe Adler; Fry, Shura";
+overviewPicture = "ga_logo.paa";
+overviewText = "Gruppe Adler Libary";
