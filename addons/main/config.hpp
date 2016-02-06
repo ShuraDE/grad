1e2a9718a5c1e0cc14b5590911ec017bfbbcd38c
ShuraDE <mail@kev-in.net>
2016-02-06 13:39:00 +0100

    .

--- addons/main/config.hpp
+++ addons/main/config.hpp
@@ -1,12 +1,15 @@
 #include "script_component.hpp"
-#include "cfgFunctions.hpp"
 
 class CfgPatches {
     class ADDON    {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {};
+        requiredAddons[] = {"grad_debug","grad_groups"};
         VERSION_CONFIG;
+        author[] = {"Fry","Shura"};
+        authorUrl = "https://gruppe-adler.de";
     };
 };
+
+#include "cfgFunctions.hpp"
