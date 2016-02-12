334201e82aa69c0f5950eb74aebc6da9a638439d
ShuraDE <mail@kev-in.net>
2016-02-13 00:36:32 +0100

    backup

--- addons/groups/XEH_postInit.sqf
+++ addons/groups/XEH_postInit.sqf
@@ -1,6 +1,6 @@
 #include "script_component.hpp"
 //addon = false;
-[\y\grad\addons\groups\functions\fnc_test.sqf, 'grad_groups_fnc_test'] call SLX_XEH_COMPILE_NEW;
+[\grad\addons\groups\functions\fnc_test.sqf, 'grad_groups_fnc_test'] call SLX_XEH_COMPILE_NEW;
 LOG_INFO("postInit");
 //PREP(test);
 //addon = true;
--- addons/groups/cfgEventhandlers.hpp
+++ addons/groups/cfgEventhandlers.hpp
@@ -1,7 +1,7 @@
 class Extended_PostInit_EventHandlers  {
     class ADDON {
         //init = call compile preprocessFileLineNumbers "XEH_postInit.sqf";
-        init = "call compile preprocessFileLineNumbers '\y\grad\addons\groups\XEH_postInit.sqf'";
+        init = "call compile preprocessFileLineNumbers '\grad\addons\groups\XEH_postInit.sqf'";
         //init = QUOTE(call COMPILE_FILE(XEH_postInit));
     };
 };
