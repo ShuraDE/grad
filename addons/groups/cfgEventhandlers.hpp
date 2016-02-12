97f168c7d91b784dfa0bba2ff1a64ea030c5b8b8
ShuraDE <mail@kev-in.net>
2016-02-12 14:06:55 +0100

    backup

--- addons/groups/cfgEventhandlers.hpp
+++ addons/groups/cfgEventhandlers.hpp
@@ -1,17 +1,6 @@
-
 class Extended_PostInit_EventHandlers  {
     class ADDON {
-        init = call compile preprocessFileLineNumbers "XEH_postInit.sqf";
-    };
-};
-class Extended_Engine_EventHandlers
-{
-    class Car
-    {
-        class TAG_MyAddon_CarEngine
-        {
-            // Like the normal engine handler above, this one runs on all machines
-            engine = "diag_log 'test'";
-        };
+        //init = call compile preprocessFileLineNumbers "XEH_postInit.sqf";
+        init = QUOTE(call COMPILE_FILE(XEH_postInit));
     };
 };
