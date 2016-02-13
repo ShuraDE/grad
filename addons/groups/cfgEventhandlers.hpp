b02253b427c31d60cdde20e2e3c19c5c17cffff7
fry-grad <fry@gruppe-adler.de>
2016-02-13 23:20:16 +0100

    change Eventhandlers

--- addons/groups/cfgEventhandlers.hpp
+++ addons/groups/cfgEventhandlers.hpp
@@ -1,7 +1,14 @@
-class Extended_PostInit_EventHandlers  {
-    class ADDON {
+class Extended_Init_EventHandlers {
+   class ADDON {
+      init = QUOTE(call COMPILE_FILE(XEH_Init));
+   };
+};
+class Extended_PostInit_EventHandlers {
+    class CAManBase {
+      class ADDON{
         //init = call compile preprocessFileLineNumbers "XEH_postInit.sqf";
         //init = "[] execVM ""XEH_postInit.sqf""";
-        init = QUOTE(call COMPILE_FILE(XEH_postInit));
+        clientinit = QUOTE(call SMIFUNC(test));                   
+      };
     };
 };
