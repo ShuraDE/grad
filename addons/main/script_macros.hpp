dc89d573f6308ec6226df0ab1f56e6e731c093d0
fry-grad <fry@gruppe-adler.de>
2016-02-14 00:00:54 +0100

    Add new init line

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -18,7 +18,7 @@
 #define CPORT DOUBLES(ADDON,comm)
 
 //activate CBA-MODUL
-#define START_XEH_FUNC = "If(isNil 'SLX_XEH_MACHINE')then{[] call CBA_fnc_preInit}";\
+#define START_XEH_FUNC init = "If(isNil 'SLX_XEH_MACHINE')then{[] call CBA_fnc_preInit}";\
 
 #define ARR_SELECT(ARRAY,INDEX,DEFAULT) if (count ARRAY > INDEX) then {ARRAY select INDEX} else {DEFAULT}
 
