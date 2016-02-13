d9332790b2a57eb27818176fede32dae243fd663
fry-grad <fry@gruppe-adler.de>
2016-02-13 23:16:56 +0100

    change Vehicle classes

--- addons/groups/cfgVehicles.hpp
+++ addons/groups/cfgVehicles.hpp
@@ -1,13 +1,4 @@
 class CfgVehicles {
-    class All {
-        class EventHandlers { EXTENDED_EVENTHANDLERS };
-    };
-  
-    // Custom object that can be used to force XEH initialization even if no XEH compatible object is on the Map.
-    class Logic;
-    class SLX_XEH_Logic: Logic {
-        displayName = "XEH (backup) Initialization Logic";
-        vehicleClass = "Modules";
-        class EventHandlers { EXTENDED_EVENTHANDLERS };
-    };
+    class Man;
+    class CAManBase: Man{};
 };
