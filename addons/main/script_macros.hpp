38e544c818572ff30b47f31a3f7f8b20bb95090d
fry-grad <fry@gruppe-adler.de>
2016-02-14 23:14:53 +0100

    add new

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -21,7 +21,7 @@
 #define COMPILE_FILE3(var1) compile preProcessFileLineNumbers var1
 
 //New PREP MACRO
-#define PRECO(var1) SMIFUNC(var1) = COMPILE_FILE3(QUOTE(PATHTOMSYS(COMPONENT,functions,DOUBLES(fnc,var1).sqf)))
+#define PRECO(var1) SMIFUNC(var1) = COMPILE_FILE3(QUOTE('PATHTOMSYS(COMPONENT,functions,DOUBLES(fnc,var1).sqf)'))
 
 #define ARR_SELECT(ARRAY,INDEX,DEFAULT) if (count ARRAY > INDEX) then {ARRAY select INDEX} else {DEFAULT}
 
