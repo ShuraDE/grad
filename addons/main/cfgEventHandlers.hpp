35178b87b880eb55e9e4931048d484ac034b96fa
fry-grad <fry@gruppe-adler.de>
2016-02-14 21:22:26 +0100

    delete Uncomments and delete compile Code

--- addons/main/cfgEventHandlers.hpp
+++ addons/main/cfgEventHandlers.hpp
@@ -2,13 +2,11 @@
 class Extended_PostInit_EventHandlers {
     class ADDON {
           serverInit = QUOTE(call COMPILE_FILE(XEH_pre_init));
-          init = QUOTE(call COMPILE_FILE(Functions\fnc_initprocess));
     };
 };
-/*
+
 class Extended_Init_EventHandlers {
     class ADDON {
           serverInit = QUOTE(call SMIFUNC(initprocess));
     };
 }
-*/
