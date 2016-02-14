fb17a0bfc50ab9ae2e624e46daeddc643529ac44
ShuraDE <mail@kev-in.net>
2016-02-14 01:16:13 +0100

    EventHandlers

--- addons/groups/cfgEventhandlers.hpp
+++ addons/groups/cfgEventhandlers.hpp
@@ -1,15 +1,5 @@
-class Extended_Init_EventHandlers {
+class Extended_PostInit_EventHandlers {
    class ADDON {
       init = QUOTE(call COMPILE_FILE(XEH_Init));
    };
-};
-class Extended_PostInit_EventHandlers {
-    class CAManBase {
-      class ADDON{
-        //init = call compile preprocessFileLineNumbers "XEH_postInit.sqf";
-        //init = "[] execVM ""XEH_postInit.sqf""";
-        //clientinit = QUOTE(call SMIFUNC(test));
-        init = QUOTE(call COMPILE_FILE(test));
-      };
-    };
 };
