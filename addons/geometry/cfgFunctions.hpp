dfaec9f1aef298b349162a28a857e0d13db892be
fry-grad <fry@gruppe-adler.de>
2016-02-21 22:03:38 +0100

    Add new Function Class

--- addons/geometry/cfgFunctions.hpp
+++ addons/geometry/cfgFunctions.hpp
@@ -4,6 +4,10 @@ class CfgFunctions
 	{
 		class geowork
 		{
+			class getCirclePos {
+														description = "Find a random Position in Circle.";
+														file = "\y\grad\addons\geometry\functions\fnc_getCirclePos.sqf";
+			                   };
       class getImpactPos {
 													 description = "Find a random Impact Position for Artillery.";
 													 file = "\y\grad\addons\geometry\functions\fnc_getImpactPos.sqf";
